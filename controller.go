59a2272104fbe6c13801ac7be8dd4a33b049d3cf
chebelok <lotokhinivan@gmail.com>
2023-02-19 17:27:13 +0200

    added log (2) by chebelok

--- controller.go
+++ controller.go
@@ -1,10 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func runApplication() error {
 	http.HandleFunc("/time", timeHandler)
 
 	const port string = ":8795"
+	log.Print("log from chebelok")
 	return http.ListenAndServe(port, nil)
 }
